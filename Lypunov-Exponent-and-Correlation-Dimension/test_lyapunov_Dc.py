9f36f821dd8b2ee0dd6670104d3db49859518a17
Sayan Nag <nagsayan112358@gmail.com>
2022-03-12 15:22:30 -0800

    updated test script

--- Lypunov-Exponent-and-Correlation-Dimension/test_lyapunov_Dc.py
+++ Lypunov-Exponent-and-Correlation-Dimension/test_lyapunov_Dc.py
@@ -3,17 +3,30 @@ from math import log
 import matplotlib.pyplot as plt
 from lyapunov_Dc_utils import *
 
-def main():
+import argparse
+
+def main(args):
     # series_data = [0.01]
     # for i in range(1000):
     #   series_data.append(series_data[i-1]*3.5*(1-series_data[i-1]))
-
+    
     t = np.linspace(1,100,1000)
-    series_data = (1/t**2)*np.sin(t)
+    
+    if args.series_type == 'oscillating':
+        series_data = np.sin(t)
+        
+    elif args.series_type == 'converging':
+        series_data = (1/t**2)*np.sin(t)
+        
+    elif args.series_type == 'diverging':
+        series_data = (t**2)*np.sin(t)
 
     LLE, Dc = Lyapunov().compute(series_data, debug_lyap_plot=False, debug_Dc_plot=False)
 
-    print('Largest Lyapunov:', LLE, ', Dc: ', Dc)
+    print('Series Type: '+args.series_type,', Largest Lyapunov:', LLE, ', Dc: ', Dc)
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser('Lyapunov_Dc')
+    parser.add_argument('--series_type', type=str, default='converging', choices=['converging', 'diverging', 'oscillating'])
+    args = parser.parse_args()
+    main(args)
